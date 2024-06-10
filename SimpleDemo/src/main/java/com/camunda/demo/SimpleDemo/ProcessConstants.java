9f41cdcf955557666be794da9ed878f9cb2be190
anushkadeshpande <anushkaa5000@gmail.com>
2024-06-10 20:22:59 +0530

    Added a demo java project with camunda maven archetype

---
+++ SimpleDemo/src/main/java/com/camunda/demo/SimpleDemo/ProcessConstants.java
@@ -0,0 +1,7 @@
+package com.camunda.demo.SimpleDemo;
+
+public class ProcessConstants {
+
+  public static final String PROCESS_DEFINITION_KEY = "SimpleDemo"; // BPMN Process ID
+
+}
---
+++ SimpleDemo/src/test/java/com/camunda/demo/SimpleDemo/ProcessUnitTest.java
@@ -0,0 +1,28 @@
+package com.camunda.demo.SimpleDemo;
+
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.test.Deployment;
+import org.camunda.community.process_test_coverage.junit5.platform7.ProcessEngineCoverageExtension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.*;
+
+/**
+ * Test case starting an in-memory database-backed Process Engine.
+ */
+@ExtendWith(ProcessEngineCoverageExtension.class)
+public class ProcessUnitTest {
+
+  @Test
+  @Deployment(resources = "process.bpmn")
+  public void testHappyPath() {
+    // Drive the process by API and assert correct behavior by camunda-bpm-assert
+
+    ProcessInstance processInstance = processEngine().getRuntimeService()
+        .startProcessInstanceByKey(ProcessConstants.PROCESS_DEFINITION_KEY);
+
+    assertThat(processInstance).isEnded();
+  }
+
+}
